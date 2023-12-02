49961cc720b91e4d160beba3566c563dfdc0a7ee
= <=>
2023-12-02 22:43:48 +0100

    updates

--- captchaScripts/script.js
+++ captchaScripts/script.js
@@ -1,33 +1,31 @@
-let direction_img = document.querySelector('.direction-img');
-let figure_img = document.querySelector('.figure-img');
-let directions_links = ['p1', 'p2', 'p3', 'p4', 'p5', 'p6'];
-let figure_links = ['1', '2', '3', '4', '5', '6'];
-let currentIndex = 0;
-let fig_index = 0;
+let $captcha_direction_img_prefix = null;
+let $captcha_figure_img_prefix = null;
+let $captcha_curr_index = 0;
 
-function putImg(imgElement, name){
-    imgElement.src = direction_img.src.replace(/p.\.JPG/i, `${name}.JPG`)
+
+function changeCaptchaDirectionImg(imgPrefix, imgNum){
+    
 }
 
 function captcha_left(){
-    currentIndex--
-    if(currentIndex < 0) currentIndex = 5;
-    putImg(direction_img, directions_links[currentIndex]);
+    $captcha_curr_index--
+    if($captcha_curr_index < 0) $captcha_curr_index = 5;
+    changeCaptchaDirectionImg($captcha_direction_img_prefix, $captcha_curr_index);
 }
 
 function captcha_right(){
-    currentIndex++
-    if(currentIndex >= 6) currentIndex = 0;
-    putImg(direction_img, directions_links[currentIndex]);
+    $captcha_curr_index++
+    if($captcha_curr_index >= 6) $captcha_curr_index = 0;
+    changeCaptchaDirectionImg($captcha_direction_img_prefix, $captcha_curr_index);
 }
 function randomize(){
     fig_index = Math.round(Math.random() * 5);
-    putImg(figure_img, figure_links[fig_index])
+    changeCaptchaDirectionImg(figure_img, figure_links[fig_index])
 }
 
 function validate(){
     let fig = figure_links[fig_index];
-    let direction = directions_links[currentIndex];
+    let direction = directions_links[$captcha_curr_index];
     console.log(fig, direction);
     if(fig == direction[1]) alert('valid');
     else alert('wrong');
@@ -109,7 +107,6 @@ function initCaptchaV1 (captchaDiv = 'captcha-root', init_h3_text = 'match the h
     rootDiv.append(captcha_inner_container, captcha_validation_section);
 
     init_imgs();
-
 }
 
 
@@ -119,10 +116,19 @@ function init_imgs(){
     let figureImg = document.querySelector('.figure-section .figure-img');
     let directionImg = document.querySelector('.direction-section .direction-img');
 
+    let randImgNum1 = Math.round(Math.random() * 5);
+    let randImgNum2 = Math.round(Math.random() * 5);
+
+    $captcha_curr_index = randImgNum1;
+
     fetch('../dummyEndPoint.json')
     .then(res => res.json())
     .then(data => {
-        directionImg.src = data.initDirectionsImg;
-        figureImg.src = data.initFigureImg;
+
+        $captcha_direction_img_prefix = data.initDirectionsImgPrefix;
+        $captcha_figure_img_prefix = data.initFigureImgPrefix;
+
+        directionImg.src = data.initDirectionsImgPrefix+randImgNum1+'.jpg';
+        figureImg.src = data.initFigureImgPrefix+randImgNum2+'.jpg';
     });
 }
